742e58ddf735f6d66060bb3200d431ec01dc131f
Ben Heavner <bheavner@gmail.com>
2014-04-11 14:52:18 -0700

    first commit, starting project

---
+++ statsguys.R
@@ -0,0 +1,5 @@
+# Following the logistic regression model approach from statsguys.wordpress.com/2014/01/03/first-post/
+
+trainData <- read.csv("data/train.csv", header = TRUE, stringsAsFactors = FALSE)
+testData <- read.csv("data/test.csv", header = TRUE, stringsAsFactors = FALSE)
+
